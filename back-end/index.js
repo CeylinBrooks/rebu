ecae210351b979d9286b6577d623538cea6e97ae
daneng1 <engeldb@gmail.com>
2021-05-14 15:32:00 -0700

    added basic code for back end

---
+++ back-end/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+require('dotenv').config();
+
+const server = require('./src/server.js');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+
+const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(MONGODB_URI, options)
+    .then(() => {
+        server.start(PORT);
+    })
+    .catch(e => console.error(e.message))
