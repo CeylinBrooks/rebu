66b42761dbf83d4df5c82de39054e7b0e01bbfb8
Brendan Smith <brendan5320@gmail.com>
2021-05-19 14:35:02 -0500

    rider dash to trip link working

--- front-end/rider/dashboard.js
+++ front-end/rider/dashboard.js
@@ -1,17 +1,8 @@
 'use strict'
 
-let map;
 
-// Initialize map 
-function initMap() {
-  const mapOptions = {
-    center: { lat: 47.6062, lng: -122.3321 },
-    zoom: 8,
-  };
-  map = new google.maps.Map(document.getElementById('map'), mapOptions);
-}
 
-// grab user loc
+// // grab user loc
 // function getLocation() {
 //   navigator.geolocation.getCurrentPosition(function (position) {
 //     directionsInfo.innerHTML = `You are here ${position.coords.latitude}, ${postion.coords.longitude}`;
@@ -26,13 +17,9 @@ $('#schedule').on('submit', function calcRoute(e) {
 
   const start = document.getElementById('start').value;
   const end = document.getElementById('end').value;
-  const request = {
-    origin: start,
-    destination: end,
-    travelMode: google.maps.TravelMode.DRIVING,
-    unitSystem: google.maps.UnitSystem.IMPERIAL
-  };
 
+  sessionStorage.setItem('start', start);
+  sessionStorage.setItem('end', end);
 
   // TODO: get rider ID (how?)
   // const rider_id = $('');
@@ -57,45 +44,13 @@ $('#schedule').on('submit', function calcRoute(e) {
   socket.emit('ride-scheduled', tripObj);
   socket.on('ride-scheduled', (trip) => {
     console.log('successful scheduling', trip);
-    // window.location.href = "/trip"
     //  TODO: redirect to trip page
   })
+  window.location.href = "/trip"
+
 })
 
-
-const directionsService = new google.maps.DirectionsService();
-
-directionsService.route(request, function (result, status) {
-  console.log(result);
-  console.log(result.routes[0].overview_path);
-  if (status === 'OK') {
-
-    // initMap(); this needs to move elsewhere
-    const polyCoords = result.routes[0].overview_path;
-    const polyBound = new google.maps.Polyline({
-      path: polyCoords,
-      strokeColor: "#FF0000",
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: "#FF0000",
-      fillOpacity: 0.35,
-    });
-    polyBound.setMap(map);
-
-
-    const cost = ((parseInt(result.routes[0].legs[0].distance.value) / 1609) * 1.75).toFixed(2);
-    console.log(cost);
-
-    const output = document.querySelector('#output');
-    output.innerHTML = "<div class='alert-info'>Pick-up: " + start + "<br />Drop-off: " + end + "<br /> Driving distance: <i class='fas fa-road'></i> " + result.routes[0].legs[0].distance.text + "<br />Estimated Duration: <i class='fas fa-hourglass-start'></i> " + result.routes[0].legs[0].duration.text + "<br />Estimated Cost: $" + cost + "</div>";
-
-  } else {
-    output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
-  }
-});
-
-
-var options = {
+const options = {
   types: ['(cities)']
 }
 
